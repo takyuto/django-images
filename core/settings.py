9630b2a30db43cf0c529237ff58aa28162cff849
takyuto <takyuto@gmail.com>
2023-08-23 10:40:27 +0900

    fix

--- core/settings.py
+++ core/settings.py
@@ -158,7 +158,7 @@ DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
 STATICFILES_STORAGE = 'core.azure_storage.AzureStaticStorage'
 
 AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
-AZURE_ACCOUNT_KEY = os.getenv('AXURE_ACCOUNT_KEY')
+AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/static/'
